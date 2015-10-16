721bc8bd854f30afb1ec831827b1970847357ac3
Karami Mohamed <karamimed@gmail.com>
2015-10-16 20:33:20 +0100

    Merge with Samsung kernel sources OI9

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Fri Oct 16 20:14:16 WEST 2015"
+#define LINUX_COMPILE_BY "wanam"
+#define LINUX_COMPILE_HOST "elksiba"
+#define LINUX_COMPILER "gcc version 4.9.3 20141031 (prerelease) (Linaro GCC 2014.11) "
