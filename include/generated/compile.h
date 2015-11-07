b474a416de680108fe5ec9d5941d3401bb7cbce8
Skin1980 <skin1980@gmail.com>
2015-11-07 09:25:03 +0100

    Update to fix compilation issue

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#1 SMP PREEMPT Fri Oct 16 20:14:16 WEST 2015"
-#define LINUX_COMPILE_BY "wanam"
-#define LINUX_COMPILE_HOST "elksiba"
+#define UTS_VERSION "#2 SMP PREEMPT Sat Nov 7 09:21:51 CET 2015"
+#define LINUX_COMPILE_BY "skin1980"
+#define LINUX_COMPILE_HOST "skin1980-virtual-machine"
 #define LINUX_COMPILER "gcc version 4.9.3 20141031 (prerelease) (Linaro GCC 2014.11) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.10.61-Adam-2.6"
+#define UTS_RELEASE "3.10.61-Skin1980-1.0"
